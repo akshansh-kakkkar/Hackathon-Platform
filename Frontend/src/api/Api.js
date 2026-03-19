f98dbd4d647449cfc5a61bdd49819952f50295de
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-03-19 18:46:25 +0530

    backend ready

--- Frontend/src/api/Api.js
+++ Frontend/src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://hackathon-platform-git-main-akshansh-kakkkars-projects.vercel.app/"
 })
 
 export const setAuthToken = (token) => {
